8093baf3c5b8288020b5b2b65a266446dfb68ca1
dillythecoder <67206248+dillythecoder@users.noreply.github.com>
2021-02-25 18:47:45 -0800

    Add files via upload

---
+++ sign up with flask py/app.py
@@ -0,0 +1,27 @@
+from flask import Flask,request,render_template
+import pickle
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template("login.html")
+database={'dillythecoder':'coding','memejets':'1234','karthik':'asdsf'}
+
+@app.route('/form_login',methods=['POST','GET'])
+def login():
+    name1=request.form['username']
+    pwd=request.form['password']
+    if name1 not in database:
+        return render_template('login.html',info='Invalid User')
+
+    else:
+        if database[name1]!=pwd:
+            return render_template('login.html',info='Invalid Password')
+        else:
+             return render_template('index.html',name=name1)
+
+if __name__ == '__main__':
+    app.run()
+
